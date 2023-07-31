8ab6bc690708575ad74e6fb9649c5bda602fe6ce
Nadushanth Sathiskumar <snadushanth2000@gmail.com>
2023-07-31 15:36:46 +0530

    user slice & store

---
+++ src/slices/userSlice.ts
@@ -0,0 +1,25 @@
+import {createSlice,createAsyncThunk} from "@reduxjs/toolkit";
+import { builtinModules } from "module";
+
+export const fetchUsers = createAsyncThunk('users/getAllUsers',async(thunkApi) => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users?_limit=5');
+    const data = await response.json();
+    return data;
+})
+
+const initialState = {
+    entities: []
+} as any
+
+const userSlice = createSlice({
+    name: 'users',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchUsers.fulfilled,(state,action) =>{
+            state.entities.push(...action.payload)
+        });
+    },
+});
+
+export default userSlice.reducer;
